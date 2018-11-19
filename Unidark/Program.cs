33a259974f07de57295d0499017d48aa248129eb
Connor McCarroll <pyrohasmc@gmail.com>
2018-11-19 14:21:35 +0000

    Add -reverse switch for command-line. Will be available in UI when I make it later

--- Unidark/Program.cs
+++ Unidark/Program.cs
@@ -6,16 +6,28 @@ namespace Unidark
 {
     class Program
     {
+        const byte LightByte = 0x75;
+        const byte DarkByte = 0x74;
+
         // Sequence from YT/DarkMental
-        static readonly byte[] SearchBytes = { 0x75, 0x08, 0x33, 0xC0, 0x48, 0x83, 0xC4, 0x20, 0x5B, 0xC3, 0x8B, 0x03, 0x48, 0x83, 0xC4, 0x20, 0x5B, 0xC3 };
+        static byte[] SearchBytes => new byte[] { IsReverseMode ? DarkByte : LightByte, 0x08, 0x33, 0xC0, 0x48, 0x83, 0xC4, 0x20, 0x5B, 0xC3, 0x8B, 0x03, 0x48, 0x83, 0xC4, 0x20, 0x5B, 0xC3 };
 
         static FileStream stream;
 
+        static bool IsReverseMode;
+
         static void Main(string[] args)
         {
-            if (args.Length == 1)
+            if (args.Any(arg => arg.ToLower() == "-reverse"))
             {
-                if (!File.Exists(args[0]))
+                IsReverseMode = true;
+            }
+
+            try
+            {
+                var unityPath = args.Single(arg => arg.Contains(".exe"));
+                
+                if (!File.Exists(unityPath))
                 {
                     Interop.ShowMessageBox("File does not exist.", MessageBoxType.Error);
                     return;
@@ -23,7 +35,7 @@ namespace Unidark
 
                 try
                 {
-                    stream = new FileStream(args[0], FileMode.Open, FileAccess.ReadWrite);
+                    stream = new FileStream(unityPath, FileMode.Open, FileAccess.ReadWrite);
                 }
                 catch
                 {
@@ -32,9 +44,9 @@ namespace Unidark
 - Click the Compatibility tab
 - Check the 'Run this program as an administrator' box
 - Try again";
-                    Interop.ShowMessageBox($"Could not open {args[0]}.\n\nIf Unity is open, please close it.\n\n{runAsAdminInstructions}", MessageBoxType.Error);
+                    Interop.ShowMessageBox($"Could not open {unityPath}.\n\nIf Unity is open, please close it.\n\n{runAsAdminInstructions}", MessageBoxType.Error);
                 }
-                
+
                 if (stream != null)
                 {
                     int offset;
@@ -47,7 +59,7 @@ namespace Unidark
                     {
                         if (IsExeFile(stream))
                         {
-                            Interop.ShowMessageBox($"Searching {Path.GetFileName(args[0])} for the offset. This might take a minute.");
+                            Interop.ShowMessageBox($"Searching {Path.GetFileName(unityPath)} for the offset. This might take a minute.");
 
                             offset = stream.GetPositionOf(SearchBytes);
                             if (offset != -1)
@@ -61,17 +73,19 @@ namespace Unidark
                         }
                         else
                         {
-                            Interop.ShowMessageBox($"{Path.GetFileName(args[0])} does not seem to be an executable file or is corrupt.", MessageBoxType.Error);
+                            Interop.ShowMessageBox($"{Path.GetFileName(unityPath)} does not seem to be an executable file or is corrupt.", MessageBoxType.Error);
                         }
                     }
 
                     stream.Dispose();
                 }
+                
             }
-            else
+            catch
             {
                 Interop.ShowMessageBox("Unidark does not run on it's own (yet). Please drop Unity.exe onto Unidark.exe.", MessageBoxType.Error);
             }
+
         }
 
         static bool IsExeFile(FileStream fs)
@@ -88,9 +102,9 @@ namespace Unidark
             try
             {
                 fs.Position = offset;
-                fs.WriteByte(0x74);
+                fs.WriteByte(IsReverseMode ? LightByte : DarkByte);
 
-                Interop.ShowMessageBox("Success! Enjoy the dark theme!");
+                Interop.ShowMessageBox($"Success! {(IsReverseMode ? "Reverted to the light theme!" : "Enjoy the dark theme!")}");
             }
             catch (Exception e)
             {
