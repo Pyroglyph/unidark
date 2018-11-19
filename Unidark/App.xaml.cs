4a3ea68481d32f4e14d1527f0b8e153dc72c3084
Connor McCarroll <pyrohasmc@gmail.com>
2018-11-19 15:51:28 +0000

    Create initial WPF interface with basic file loading

---
+++ Unidark/App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace Unidark
+{
+    public partial class App
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Stream stream = null;
+
+            if (!e.Args.Any())
+            {
+                var dialog = new OpenFileDialog
+                {
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+                    Filter = "Unity Editor|*.exe",
+                    Multiselect = false,
+                    InitialDirectory = "C:\\Program Files\\Unity\\Editor",
+                    DefaultExt = "exe",
+                    FileName = "C:\\Program Files\\Unity\\Editor\\Unity.exe"
+                };
+
+                if (dialog.ShowDialog() == true)
+                    stream = dialog.OpenFile();
+                else
+                    Environment.Exit(1);
+            }
+            else
+            {
+                string exePath = null;
+                try
+                {
+                    exePath = e.Args.Single(arg => arg.Contains(".exe"));
+                }
+                catch
+                {
+                    ShowFatalError("An exectable was not provided.\nTry dropping Unity.exe onto Unidark.exe");
+                }
+
+                if (exePath != null)
+                try
+                {
+                    stream = new FileStream(exePath, FileMode.Open, FileAccess.ReadWrite);
+                }
+                catch
+                {
+                    ShowFatalError($"Failed to open {exePath}.\nTry running Unidark as an administrator.");
+                }
+            }
+
+            if (stream != null)
+            {
+                var mainWindow = new MainWindow(stream);
+                mainWindow.Show();
+            }
+            else
+            {
+                ShowFatalError("Something has gone very very wrong!");
+            }
+        }
+
+        public static void ShowFatalError(string message)
+        {
+            MessageBox.Show(message, "Unidark - Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ Unidark/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace Unidark
+{
+    public partial class MainWindow
+    {
+        private Stream stream { get; }
+
+        public static bool IsComplete { get; set; }
+
+        public string Status { get; set; }
+        public int MainStageProgress { get; set; }
+        public int CurrentStageProgress { get; set; }
+        public Visibility SuccessMessageVisibility { get; set; } = IsComplete ? Visibility.Visible : Visibility.Collapsed;
+        
+        public MainWindow(Stream exeStream)
+        {
+            stream = exeStream;
+
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
